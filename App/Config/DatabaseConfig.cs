efa04be2e6791bc6479d7dc41d6c6418a2184614
DerianCordobaPerez <derianricardo451@gmail.com>
2024-07-28 17:56:19 -0600

    feat: Updated the database config file

--- App/Config/DatabaseConfig.cs
+++ App/Config/DatabaseConfig.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DegreeExamen.App.Config
 {
     internal sealed class DatabaseConfig
     {
+        public static string DatabaseName
+        {
+            get => Environment.GetEnvironmentVariable("DATABASE_NAME");
+        }
+
+        public static string DatabaseServer
+        {
+            get => Environment.GetEnvironmentVariable("DATABASE_SERVER");
+        }
+
+        public static string DatabaseSecurity
+        {
+            get => Environment.GetEnvironmentVariable("DATABASE_SECURITY") ?? "true";
+        }
+
         public static string ConnectionURL
         {
-            get => Environment.GetEnvironmentVariable("DATABASE_URL");
+            // We need to replace the pattern template in the URL to connect to database server.
+            // 
+            // Default URL template: server={SERVER}; database={DATABASE}; integrated security=true
+            // Using the `Replace` method, we can change the template values for the real values.
+            get => ConnectionURLTemplate
+                .Replace("{DATABASE_SERVER}", DatabaseServer)
+                .Replace("{DATABASE_NAME}", DatabaseName)
+                .Replace("{DATABASE_SECURITY}", DatabaseSecurity);
         }
+
+        // Connection URL template for SQL Server express
+        private static readonly string ConnectionURLTemplate = "server={DATABASE_SERVER}; database={DATABASE_NAME}; integrated security={DATABASE_SECURITY}";
     }
 }
