599be781767e683bdc948770fa4efbbc13649568
DerianCordobaPerez <derianricardo451@gmail.com>
2024-07-28 22:36:04 -0600

    chore: Updated the URL connection

--- App/Config/DatabaseConfig.cs
+++ App/Config/DatabaseConfig.cs
@@ -21,17 +21,7 @@ namespace DegreeExamen.App.Config
 
         public static string ConnectionURL
         {
-            // We need to replace the pattern template in the URL to connect to database server.
-            // 
-            // Default URL template: server={SERVER}; database={DATABASE}; integrated security=true
-            // Using the `Replace` method, we can change the template values for the real values.
-            get => ConnectionURLTemplate
-                .Replace("{DATABASE_SERVER}", DatabaseServer)
-                .Replace("{DATABASE_NAME}", DatabaseName)
-                .Replace("{DATABASE_SECURITY}", DatabaseSecurity);
+            get => $"server={DatabaseServer}; database={DatabaseName}; integrated security={DatabaseSecurity}";
         }
-
-        // Connection URL template for SQL Server express
-        private static readonly string ConnectionURLTemplate = "server={DATABASE_SERVER}; database={DATABASE_NAME}; integrated security={DATABASE_SECURITY}";
     }
 }
